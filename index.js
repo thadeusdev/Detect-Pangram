b7578c3aa8dd83bbad6b840e6b830b55e73e43be
thadeusdev <109853680+thadeusdev@users.noreply.github.com>
2022-09-12 19:22:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-function isPangram(string) {
-    const alphabet = "abcdefghijklmnopqrstuvwxyz";
-    const regex = /\string/g;
-    const lowercase = string.toLowerCase().replace(regex, "");
-   
-    for(let i = 0; i < alphabet.length; i++){
-     if(lowercase.indexOf(alphabet[i]) === -1){
-       return "is not pangram";
-     }
-    }
-   
-   return "is a pangram";
- }
+//Detect Pangram
+function isPangram(string){
+
+    // character set capturing group with negative lookahead
+    let regex = /([a-z])(?!.*\1)/gi;
+    return (string.match(regex)).length === 26; 
+}
+console.log(isPangram("The quick brown fox jumps over the lazy dog."));
